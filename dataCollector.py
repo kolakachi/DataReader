ec098b5bc14a97f86fe39493c74f67ca857f6e8c
Kolawole <kola.lawal91@yahoo.com>
2017-10-22 10:40:07 +0100

    Update dataCollector.py

--- dataCollector.py
+++ dataCollector.py
@@ -1,10 +1,11 @@
 import csv
 
+Result = []
 Counter = 0
 FileToRead = open('slatecode test.csv','rU')
 FileReader = csv.reader(FileToRead)
 for Row in FileReader:
-        
+    Result.append(Row[Counter])    
     print (Row[Counter])
     Counter+=1
         
